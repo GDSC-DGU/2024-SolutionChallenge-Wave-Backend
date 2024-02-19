bc4bc4e69976a543aa851e2427c85168ca8e3359
jjuuuunnii <129721531+jjuuuunnii@users.noreply.github.com>
2024-02-20 05:25:35 +0900

    Feat: Delete flask/config.py

--- flask/config.py
+++
@@ -1,5 +0,0 @@
-CLIENT_ID = "PFPJZWeAJDQwd3CYRkAt"
-CLIENT_SECRET = "xNOdafoKsz"
-PROD_SERVER = "http://springapp:8080/api/v1/countries/crawling-news"
-
-LOCAL_SERVER = "http://localhost:8080/api/v1/countries/crawling-news"
